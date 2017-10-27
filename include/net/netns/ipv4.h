eca357791faac779205231e618715f9fba724c02
Eric Dumazet <edumazet@google.com>
2017-10-26 21:54:57 -0700

    tcp: Namespace-ify sysctl_tcp_recovery
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/net/netns/ipv4.h
+++ include/net/netns/ipv4.h
@@ -132,6 +132,7 @@ struct netns_ipv4 {
 	int sysctl_tcp_timestamps;
 	int sysctl_tcp_default_init_rwnd;
 	int sysctl_tcp_early_retrans;
+	int sysctl_tcp_recovery;
 	struct inet_timewait_death_row tcp_death_row;
 	int sysctl_max_syn_backlog;
 	int sysctl_tcp_fastopen;
